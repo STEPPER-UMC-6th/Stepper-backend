eab6f37c542970f5ca73b0e00ba4049cac3be332
hyojin425 <gywls5184@gmail.com>
2024-07-11 19:36:12 +0900

    init

---
+++ src/main/java/com/example/stepperbackend/StepperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.stepperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepperBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stepperbackend/StepperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stepperbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
