ee8a23c0f40b1b8d4c95f90bf581116fa67aaef2
walesmin <walesmin@naver.com>
2024-07-20 15:38:11 +0900

    [Feat] 회원가입 컨트롤러 추가

---
+++ src/main/java/com/example/stepperbackend/web/controller/MemberController.java
@@ -0,0 +1,39 @@
+package com.example.stepperbackend.web.controller;
+
+import com.example.stepperbackend.service.MemberService.MemberService;
+import com.example.stepperbackend.web.dto.MemberDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/members")
+public class MemberController {
+
+    @Autowired
+    private MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberDto.MemberResponseDto> signup(@RequestBody MemberDto.MemberSignupRequestDto dto) {
+        MemberDto.MemberResponseDto response = memberService.signup(dto);
+        return ResponseEntity.ok(response);
+    }
+
+//    @PostMapping("/login")
+//    public ResponseEntity<MemberDto.MemberResponseDto> login(@RequestBody MemberDto.MemberLoginRequestDto dto) {
+//        MemberDto.MemberResponseDto response = memberService.login(dto);
+//        return ResponseEntity.ok(response);
+//    }
+//
+//    @GetMapping("/{memberId}")
+//    public ResponseEntity<MemberDto.MemberResponseDto> getUserInfo(@PathVariable Long memberId) {
+//        MemberDto.MemberResponseDto response = memberService.getUserInfo(memberId);
+//        return ResponseEntity.ok(response);
+//    }
+//
+//    @DeleteMapping("/{memberId}")
+//    public ResponseEntity<Void> deleteUser(@PathVariable Long memberId, @RequestBody String password) {
+//        memberService.deleteUser(memberId, password);
+//        return ResponseEntity.noContent().build();
+//    }
+}
