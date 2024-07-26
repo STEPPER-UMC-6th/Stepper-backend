63f3b43cf03f45546fddcce7a6ce447c139ab5d7
walesmin <walesmin@naver.com>
2024-07-26 11:34:36 +0900

    [FEAT] SecurityConfig 의 세션을 STATELESS 상태로 설정

--- src/main/java/com/example/stepperbackend/config/SecurityConfig.java
+++ src/main/java/com/example/stepperbackend/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -15,34 +15,63 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
 
     @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .csrf((auth) -> auth.disable());
 
-                .csrf(AbstractHttpConfigurer::disable)
+        http
+                .formLogin((auth) -> auth.disable());
+
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/login", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/api/members/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
-                        .anyRequest().authenticated()
-                )
+                        .anyRequest().authenticated());
 
-                .logout((logout) -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/api/members/logout"))
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true))
-
-                ;
-
-
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
 
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http
+//
+//                //.csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests((auth) -> auth
+//                        .requestMatchers("/", "/login", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+//                        .requestMatchers("/api/members/**").permitAll()
+//                        .requestMatchers("/admin").hasRole("ADMIN")
+//                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+//                        .anyRequest().authenticated()
+//                )
+//
+//                .logout((logout) -> logout
+//                        .logoutRequestMatcher(new AntPathRequestMatcher("/api/members/logout"))
+//                        .logoutSuccessUrl("/")
+//                        .invalidateHttpSession(true))
+//
+//                ;
+//
+//
+//
+//        return http.build();
+//    }
+
 }
