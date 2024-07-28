56a07cea73926e2399f6586dd3407c7dc6f9b6d1
hyojin425 <gywls5184@gmail.com>
2024-07-28 20:26:44 +0900

    [Fix] 추가 운동 기록/조회 로그인 인증 구현

--- src/main/java/com/example/stepperbackend/web/controller/MoreExerciseController.java
+++ src/main/java/com/example/stepperbackend/web/controller/MoreExerciseController.java
@@ -1,9 +1,11 @@
 package com.example.stepperbackend.web.controller;
 
 import com.example.stepperbackend.apiPayload.ApiResponse;
+import com.example.stepperbackend.jwt.JWTUtil;
 import com.example.stepperbackend.service.moreExerciseService.MoreExerciseService;
 import com.example.stepperbackend.web.dto.MoreExerciseDto;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -18,19 +20,26 @@ import java.util.List;
 public class MoreExerciseController {
 
     final MoreExerciseService moreExerciseService;
+    private final JWTUtil jwtUtil;
+
 
     @Operation(summary = "추가 운동 기록 API",description = "추가 운동 기록")
     @PostMapping("/more")
-    public ApiResponse<MoreExerciseDto.MoreExerciseResponseDto> exerciseAdd(@RequestBody MoreExerciseDto.MoreExerciseRequestDto dto, @RequestParam String email) {
+    public ApiResponse<MoreExerciseDto.MoreExerciseResponseDto> exerciseAdd(@RequestBody MoreExerciseDto.MoreExerciseRequestDto dto, HttpServletRequest request) {
 
+        String token = request.getHeader("Authorization").substring(7);
+        String email = jwtUtil.getUsername(token);
         MoreExerciseDto.MoreExerciseResponseDto response = moreExerciseService.exerciseAdd(dto,email);
         return ApiResponse.onSuccess(response);
     }
 
     @Operation(summary = "추가 운동 기록 조회 API",description = "추가 운동 기록 조회")
     @GetMapping("/more")
-    public ApiResponse<List<MoreExerciseDto.MoreExerciseResponseDto>> getMoreExercise(@RequestParam String email, @RequestParam LocalDate date) {
+    public ApiResponse<List<MoreExerciseDto.MoreExerciseResponseDto>> getMoreExercise(@RequestParam LocalDate date, HttpServletRequest request) {
 
+        System.out.println("MoreExerciseController.getMoreExercise");
+        String token = request.getHeader("Authorization").substring(7);
+        String email = jwtUtil.getUsername(token);
         List<MoreExerciseDto.MoreExerciseResponseDto> response = moreExerciseService.getMoreExerciseList(email, date);
         return ApiResponse.onSuccess(response);
     }
