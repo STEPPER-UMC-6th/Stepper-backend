964f3973187225158be9b928b846eb73dbf9c673
walesmin <walesmin@naver.com>
2024-08-02 18:06:43 +0900

    [FIX] 게시글 도메인, 서브카테고리 생성

---
+++ src/main/java/com/example/stepperbackend/domain/Post.java
@@ -0,0 +1,35 @@
+package com.example.stepperbackend.domain;
+
+import com.example.stepperbackend.domain.common.BaseEntity;
+import com.example.stepperbackend.domain.enums.SubCategory;
+import jakarta.persistence.Entity;
+import lombok.*;
+import jakarta.persistence.*;
+
+
+@Entity
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+    private String title;
+    private String body;
+
+    private Long categoryId;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private SubCategory subCategory;
+}
