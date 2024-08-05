3338e009ca7c1cc2e81f4d90398aa5011c367227
walesmin <walesmin@naver.com>
2024-08-05 19:22:09 +0900

    [FEAT] 첫 게시글 작성시 뱃지 추가

--- src/main/java/com/example/stepperbackend/repository/PostRepository.java
+++ src/main/java/com/example/stepperbackend/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package com.example.stepperbackend.repository;
 
+import com.example.stepperbackend.domain.Member;
 import com.example.stepperbackend.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT count(e) FROM Post e WHERE e.member = :member")
+    int getCountByMember(@Param("member") Member member);
 }
